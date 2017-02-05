a2e0c29169f3f0408c34ee307789eca9a0751e63
Sebastian Puskeiler <s.puskeiler@mytaxi.com>
2017-02-05 18:04:37 +0100

    * inital commit

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+const alfy = require('alfy');
+const alfredNotifier = require('alfred-notifier');
+const request = require('request');
+const parse = require('csv-parse');
+
+const awsUri = alfy.config.get('cooper-awsUri');
+const csvFile = alfy.config.get('cooper-csvFileUri');
+const auth = alfy.config.get('cooper-authToken');
+
+alfredNotifier();
+
+var options = {
+	url: csvFile,
+	headers: {
+		'Authorization': 'Basic ' + auth
+	}
+};
+
+function callback(error, response, body) {
+	if (!error && response.statusCode == 200) {
+		var projects = body.split('\n');
+		projects.pop();
+
+		var items = projects.map(x => ({
+			title: x.split(',')[0],
+			subtitle: 'Prio: ' + x.split(",")[1] + ', Port: ' + x.split(",")[2],
+			arg: awsUri + '{ENV}-Prio' + x.split(',')[1].charAt(0).toUpperCase() + x.split(',')[1].slice(1).toLowerCase() + '/services/{ENV}-' + x.split(',')[0] + '/tasks'
+		}));
+		alfy.output(items);
+	}
+}
+
+request(options, callback);
---
+++ readme.md
@@ -0,0 +1,22 @@
+# cooper [![Build Status](https://travis-ci.org/codebud7/cooper.svg?branch=master)](https://travis-ci.org/codebud7/cooper)
+
+> Search for services in ECS cluster.
+
+
+## Install
+
+```
+$ npm install -g cooper
+```
+
+*Requires [Node.js](https://nodejs.org) 4+ and the Alfred [Powerpack](https://www.alfredapp.com/powerpack/).*
+
+
+## Usage
+
+In Alfred, type `coo`, <kbd>Enter</kbd>, and your query.
+
+
+## License
+
+MIT © [Sebastian Puskeiler](https://github.com/codeBud7)
