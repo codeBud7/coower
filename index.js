bcec7c50138c9362e9f49fd16c9d273fe2a95db8
Sebastian Puskeiler <s.puskeiler@mytaxi.com>
2017-02-10 09:22:50 +0100

    * new way of configuration

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const alfredNotifier = require('alfred-notifier');
 const request = require('request');
 const parse = require('csv-parse');
 
-const awsUri = alfy.config.get('coower-awsUri');
 const csvFile = alfy.config.get('coower-csvFileUri');
 const auth = alfy.config.get('coower-authToken');
 
@@ -27,7 +26,7 @@ function callback(error, response, body) {
 		}).map(x => ({
 			title: x.split(',')[0],
 			subtitle: 'Prio: ' + x.split(",")[1] + ', Port: ' + x.split(",")[2],
-			arg: awsUri + '{ENV}-Prio' + x.split(',')[1].charAt(0).toUpperCase() + x.split(',')[1].slice(1).toLowerCase() + '/services/{ENV}-' + x.split(',')[0] + '/tasks'
+			arg: 'https://eu-west-1.console.aws.amazon.com/ecs/home?region=eu-west-1#/clusters/{ENV}-Prio' + x.split(',')[1].charAt(0).toUpperCase() + x.split(',')[1].slice(1).toLowerCase() + '/services/{ENV}-' + x.split(',')[0] + '/tasks'
 		}));
 		alfy.output(items);
 	}
